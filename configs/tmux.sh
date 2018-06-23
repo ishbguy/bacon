9a9e7d9353d6c3143d0ab86971d8788cb6f6e53e
Herbert Shen <ishbguy@hotmail.com>
2018-06-23 21:10:25 +0800

    add tmux config

---
+++ configs/tmux.sh
@@ -0,0 +1,35 @@
+#! /usr/bin/env bash
+# Copyright (c) 2018 Herbert Shen <ishbguy@hotmail.com> All Rights Reserved.
+# Released under the terms of the MIT License.
+
+for req in tmux; do
+    hash "$req" &>/dev/null || return 1
+done
+
+export BASH_CONFIG_TMUX_SRC="$(realpath "${BASH_SOURCE[0]}")"
+export BASH_CONFIG_TMUX_DIR="$(dirname "$BASH_CONFIG_TMUX_SRC")"
+
+#alias for tmux
+alias tmx='tmux'
+alias tmxn='tmux new-session'
+alias tmxl='tmux list-sessions'
+alias tmxa='tmux attach-session'
+alias tmxk='tmux kill-session'
+alias tmxc='tmux capture-pane'
+
+# Some functions for tmux
+tma() { tmux attach-session "$1"; }
+tmd() {
+    tmux new-session -s "${1:Dev}" -n "${2:-Coding}" \
+        -c ~/exer -d \; split-window -c ~/doc -h -d \; attach
+}
+
+if hash mutt &>/dev/null && hash offlineimap &>/dev/null; then
+    tmm() {
+        tmux new-session -s "${1:-Com}" -n "${2:-Mutt}" \
+            -c ~/.mutt/attach/ -d 'mutt' \; \
+            new-window -n Offlineimap -c ~/.mail/hotmail 'offlineimap' \; attach
+    }
+fi
+
+# vim:set ft=sh ts=4 sw=4:
