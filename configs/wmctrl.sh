b64302ee3e4e00647b7ecf3449adc3310dfedf79
Herbert Shen <ishbguy@hotmail.com>
2018-06-23 21:37:11 +0800

    add wmctrl config

---
+++ configs/wmctrl.sh
@@ -0,0 +1,16 @@
+#! /usr/bin/env bash
+# Copyright (c) 2018 Herbert Shen <ishbguy@hotmail.com> All Rights Reserved.
+# Released under the terms of the MIT License.
+
+hash wmctrl &>/dev/null || return 1
+
+export BASH_CONFIG_WMCTRL_SRC="$(realpath "${BASH_SOURCE[0]}")"
+export BASH_CONFIG_WMCTRL_DIR="$(dirname "$BASH_CONFIG_WMCTRL_SRC")"
+
+alias fullscreen='wmctrl -i -r $WINID -b toggle,fullscreen'
+
+# get x window id
+[[ -z $WINID && -n $DISPLAY ]] && export WINID=$(wmctrl -l | tail -n1 | awk '{print $1}')
+
+
+# vim:set ft=sh ts=4 sw=4:
