e3d1f4c69940e2be6570156c87a00d17e06c2b6a
Herbert Shen <ishbguy@hotmail.com>
2018-06-23 21:51:41 +0800

    add first time install script

---
+++ install.sh
@@ -0,0 +1,23 @@
+#! /usr/bin/env bash
+# Copyright (c) 2018 Herbert Shen <ishbguy@hotmail.com> All Rights Reserved.
+# Released under the terms of the MIT License.
+
+# source guard
+[[ $INSTALL_SOURCED -eq 1 ]] && return
+declare -r INSTALL_SOURCED=1
+declare -r INSTALL_ABS_SRC="$(realpath "${BASH_SOURCE[0]}")"
+declare -r INSTALL_ABS_DIR="$(dirname "$INSTALL_ABS_SRC")"
+
+install() {
+    local -a configs=(bashrc bash_profile bash_logout)
+    local date=$(date +%Y%m%d)
+    for cfg in "${configs[@]}"; do
+        [[ -f $HOME/.$cfg ]] && mv "$HOME/.$cfg" "$HOME/.$cfg-$date"
+        ln -s "$INSTALL_ABS_DIR/$cfg" "$HOME/.$cfg"
+    done
+}
+
+[[ ${FUNCNAME[0]} == "main" ]] \
+    && install "$@"
+
+# vim:set ft=sh ts=4 sw=4:
